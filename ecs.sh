afe0f825b436a9f6921eb98f6496af763e17c616
Radheykant <rbhardwa@gmail.com>
2024-09-21 18:43:13 +0530

    again update

--- ecs.sh
+++ ecs.sh
@@ -37,15 +37,14 @@ systemctl start ecs
 systemctl enable ecs
 
 
-$ # Set up directories the agent uses
-$ mkdir -p /var/log/ecs /etc/ecs /var/lib/ecs/data
-$ touch /etc/ecs/ecs.config
-$ # Set up necessary rules to enable IAM roles for tasks
-$ sysctl -w net.ipv4.conf.all.route_localnet=1
-$ iptables -t nat -A PREROUTING -p tcp -d 169.254.170.2 --dport 80 -j DNAT --to-destination 127.0.0.1:51679
-$ iptables -t nat -A OUTPUT -d 169.254.170.2 -p tcp -m tcp --dport 80 -j REDIRECT --to-ports 51679
-$ # Run the agent
-$ docker run --name ecs-agent \
+ # Set up directories the agent uses
+ mkdir -p /var/log/ecs /etc/ecs /var/lib/ecs/data
+ # Set up necessary rules to enable IAM roles for tasks
+ sysctl -w net.ipv4.conf.all.route_localnet=1
+# iptables -t nat -A PREROUTING -p tcp -d 169.254.170.2 --dport 80 -j DNAT --to-destination 127.0.0.1:51679
+# iptables -t nat -A OUTPUT -d 169.254.170.2 -p tcp -m tcp --dport 80 -j REDIRECT --to-ports 51679
+ # Run the agent
+ docker run --name ecs-agent \
     --detach=true \
     --restart=on-failure:10 \
     --volume=/var/run/docker.sock:/var/run/docker.sock \
